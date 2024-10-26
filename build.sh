220ffbde4b601744f259ccdf298ad65ca6cf1b51
usrfriendly <2502060+usrfriendly@users.noreply.github.com>
2024-10-25 23:56:57 -0700

    Update build.sh
    Adding krdp

--- build.sh
+++ build.sh
@@ -15,6 +15,7 @@ RELEASE="$(rpm -E %fedora)"
 # this installs a package from fedora repos
 
 rpm-ostree install waydroid
+rpm-ostree install krdp
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
